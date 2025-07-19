e7fa3bc5c6c5ca5dc4772a9573d74600648d6106
janvi <janvi7048962683@gmail.com>
2025-07-19 16:16:08 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -7,7 +7,7 @@ def chatbot():
         if user_input in ['hi', 'hello']:
             print("Chatbot: Hi there! How can I assist you?")
         elif "your name" in user_input:
-            print("Chatbot: I'm a Rule-Based Chatbot created by Tamanna!")
+            print("Chatbot: I'm a Rule-Based Chatbot created by janvi!")
         elif "how are you" in user_input:
             print("Chatbot: I'm just a program, but I'm functioning as expected!")
         elif "bye" in user_input:
